d9648ea079e4de660d6a925fc48c445263533b39
rodrigocamargo93 <rodrigo.93camargo@gmail.com>
2024-03-26 19:57:51 -0300

    atualização - valor minimo e maximo

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const button = document.querySelector("button")
 button.addEventListener("click", generateNumber)
 
-function generateNumber(){
-    
+function generateNumber() {
+
     const min = Math.ceil(document.querySelector(".input-a").value)
     const max = Math.floor(document.querySelector(".input-b").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min
-    
-    alert(result)
+    if (min >= max) {
+        alert('ERROR - O primeiro valor tem que ser MENOR que o segundo!!')
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min
+
+        alert(result)
+    }
+
 
 }
